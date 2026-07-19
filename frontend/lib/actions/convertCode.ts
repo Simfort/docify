559345d0dff3586ee5e9ad357f70264ab691354b
Simfort <lemondzavadavid265@gmail.com>
2026-07-19 12:02:30 +0300

    added catch errors

--- frontend/lib/actions/convertCode.ts
+++ frontend/lib/actions/convertCode.ts
@@ -26,28 +26,22 @@ export async function convertCode({
     } else {
       throw new Error("File is not defined");
     }
-  } else if (mode === 2) {
-    return aiFetch(file!);
   }
 }
 
 export const fileFetch = async ({ file, ai, root }: Partial<ParamsConvert>) => {
-  const formData = new FormData();
-  formData.append("file", file!);
-  formData.append("root", String(root));
-  formData.append("ai", String(ai));
-  const response = await fetch(`${checkBackendAPI()}/gen/files`, {
-    method: "POST",
-    keepalive: true,
-    body: formData,
-  });
-  return response.json();
-};
-export const aiFetch = async (file: File) => {
-  const response = await fetch(`${checkBackendAPI()}/gen/ai`, {
-    method: "POST",
-
-    body: JSON.stringify({ file }),
-  });
-  return response.json();
+  try {
+    const formData = new FormData();
+    formData.append("file", file!);
+    formData.append("root", String(root));
+    formData.append("ai", String(ai));
+    const response = await fetch(`${checkBackendAPI()}/gen/files`, {
+      method: "POST",
+      keepalive: true,
+      body: formData,
+    });
+    return response.json();
+  } catch (error) {
+    console.error(error);
+  }
 };
